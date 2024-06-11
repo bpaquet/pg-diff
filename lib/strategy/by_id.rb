8853a32ba2ea7ef5138978af9d637cee3228e41a
Bertrand Paquet <bertrand.paquet@gmail.com>
2024-06-11 23:42:07 +0200

    Pfff

--- lib/strategy/by_id.rb
+++ lib/strategy/by_id.rb
@@ -66,8 +66,8 @@ module Strategy
       current = key_start
       while current < build_next_key(key_stop, @options[:batch_size])
         next_current = build_next_key(current, @options[:batch_size])
-        next_current = [next_current, str_to_key(@options[:key_stop])].min if @options[:key_stop]
-        result << build_batch(current, next_current)
+        end_batch =  @options[:key_stop] ? [next_current, str_to_key(@options[:key_stop])].min : next_current
+        result << build_batch(current, end_batch)
         current = next_current
       end
       logger.info("[#{@table}] First batch: #{result.first[:where]}, last batch: #{result.last[:where]}")
