62ff6246a923801cd81817a4e1206703c4d6679c
Bertrand Paquet <bertrand.paquet@gmail.com>
2024-06-12 07:51:10 +0200

    Improve logs

--- lib/table_comparer.rb
+++ lib/table_comparer.rb
@@ -98,7 +98,8 @@ class TableComparer
       puts File.read(diff_file.path)
       diffs = @extract_result_helper.parse(diff_file.path)
       pks = diffs.map { |diff| diff.values.first }.uniq.sort
-      logger.info("[#{table}] Error found on batch #{batch[:name]}, rechecking some lines: #{pks.join(',')}")
+      logger.warn("[#{table}] Error found on batch #{batch[:name]}, rechecking some lines: " \
+                  "#{columns.first} in #{pks.join(',')}")
       sleep(options[:recheck_for_errors])
       new_batch = {
         name: "#{batch[:name]}_recheck",
