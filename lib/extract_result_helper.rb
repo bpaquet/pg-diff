c1dcdbdc4a37f37aa6c5be3c72604ec1754e4eb5
Bertrand Paquet <bertrand.paquet@gmail.com>
2025-03-21 08:51:05 +0100

    Fix extract result with consecutive diffs

--- lib/extract_result_helper.rb
+++ lib/extract_result_helper.rb
@@ -11,24 +11,32 @@ class ExtractResultHelper
     FileUtils.rm_f(@filename) if @filename
   end
 
-  def parse(log_file)
+  def extract_key(line)
+    line[2..].split("\t").first
+  end
+
+  def build_output(outs, ins)
     outputs = []
-    last_key = nil
-    File.readlines(log_file).map(&:strip).each do |line|
-      if line.start_with?('< ')
-        last_key = line[2..].split("\t").first
-        outputs << { only_in_source: last_key }
-      elsif line.start_with?('> ')
-        new_key = line[2..].split("\t").first
-        if last_key == new_key
-          outputs[-1] = { changed: new_key }
-        else
-          last_key = new_key
-          outputs << { only_in_target: line[2..].split("\t").first }
-        end
-      end
+    outputs += outs.map do |key, _|
+      ins[key] ? { 'changed' => key } : { 'only_in_source' => key }
+    end
+    ins.each_key do |key|
+      outputs << { 'only_in_target' => key } unless outs[key]
     end
     outputs
+  end
+
+  def parse(log_file)
+    outs = {}
+    ins = {}
+    File.readlines(log_file).map(&:strip).each do |line|
+      if line.start_with?('< ')
+        outs[extract_key(line)] = true
+      elsif line.start_with?('> ')
+        ins[extract_key(line)] = true
+      end
+    end
+    build_output(outs, ins)
   end
 
   def process(log_file)
